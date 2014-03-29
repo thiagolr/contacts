483b40cf8288590068daaad0827455f844d109d9
ios4341 <ios4341@caelum57-14.local>
2014-03-29 09:52:29 -0300

    first view controller created

---
+++ Contacts/FormularioContatoViewController.h
@@ -0,0 +1,13 @@
+//
+//  FormularioContatoViewController.h
+//  Contacts
+//
+//  Created by ios4341 on 29/03/14.
+//  Copyright (c) 2014 Venturus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FormularioContatoViewController : UIViewController
+
+@end
---
+++ Contacts/FormularioContatoViewController.m
@@ -0,0 +1,38 @@
+//
+//  FormularioContatoViewController.m
+//  Contacts
+//
+//  Created by ios4341 on 29/03/14.
+//  Copyright (c) 2014 Venturus. All rights reserved.
+//
+
+#import "FormularioContatoViewController.h"
+
+@interface FormularioContatoViewController ()
+
+@end
+
+@implementation FormularioContatoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
